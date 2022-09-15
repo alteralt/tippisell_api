36d9eb0f1fba03ea551bb2a09e1fb58bda161f24
tippisell <108689506+tippisell@users.noreply.github.com>
2022-09-15 14:37:08 +0300

    Add files via upload

---
+++ examples/validate_sell.py
@@ -0,0 +1,26 @@
+import random
+import typing
+from aiohttp import web
+from tippisell_api import webhook, models
+
+
+class WebhookRequestHandler(webhook.WebhookRequestHandler):
+    async def validate_sale(
+            self, user: models.User, product: models.Product, data: typing.List[str]) -> typing.List[dict]:
+        response = bool(random.randint(0, 1))
+        print(response)
+        return [{"is_valid": response, "data": line} for line in data]
+
+
+routes = [
+    web.post("/", WebhookRequestHandler)
+]
+
+
+app = web.Application()
+app.add_routes(routes)
+
+
+if __name__ == "__main__":
+    app["_check_ip"] = False
+    web.run_app(app, host="127.0.0.1", port=5000)
