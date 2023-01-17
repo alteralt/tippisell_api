6a9555758a04022da41b278720253911a3959f02
daveusa <109542778+alteralt@users.noreply.github.com>
2023-01-17 20:02:51 +0300

    Delete clients

--- tippisell_api/__init__.py
+++ tippisell_api/__init__.py
@@ -1,1 +1,17 @@
-from . import exceptions
+from . import exceptions, client
+
+import sys
+tippisell_api = sys.modules[__name__]
+setattr(tippisell_api, "clients", client)
+print("эм")
+
+print(tippisell_api)
+
+sys.modules[__name__] = tippisell_api
+
+
+
+clients = type("clients", (object,), {"async_": client})
+setattr(tippisell_api, "clients", clients)
+print("эм")
+sys.modules[__name__] = tippisell_api
